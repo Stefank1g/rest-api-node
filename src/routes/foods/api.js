5c4975b6c5a0774b46591a1266cd28726e8aa4dc
Stefan Kostadinoski <skostadinoski00@gmail.com>
2023-09-19 10:42:32 +0200

    Add endpoints

--- src/routes/foods/api.js
+++ src/routes/foods/api.js
@@ -1,58 +1,71 @@
 const FoodModel = require("../../models/food.js");
 
 module.exports = {
-  
   createNewFood: function (name, calories, description, callback) {
     const newFood = new FoodModel({
       name: name,
       calories: calories,
       description: description,
-  });
-},
-    
-    getAllFoods: function(callback) {
-    FoodModel.find({}).lean().exec(function(error, foods) {
-      if (error) {
-        callback({error: true})
-      } else {
-        callback({success: true, foods: foods})
-      }
-    })
+    });
+
+    newFood
+      .save()
+      .then((newItem) => {
+        callback({ success: true });
+        console.log("Created new item: ", newItem);
+      })
+      .catch(() => {
+        callback({ error: true });
+      });
   },
 
-changeFoodDescription: function(name, newDescription, callback) {
-  FoodModel.findOne({name: name}).exec(function(error, food) {
-    if (error) {
-      callback({error: true})
-    } else if (!food) {
-      callback({notFoundError: true})
-    } else {
-      food.description = newDescription
+  getAllFoods: function (callback) {
+    FoodModel.find({})
+      .lean()
+      .exec(function (error, foods) {
+        if (error) {
+          callback({ error: true });
+        } else {
+          callback({ success: true, foods: foods });
+        }
+      });
+  },
 
-      food.save(function(error) {
-        if (error) {
-          callback({error: true})
-        } else {
-          callback({success: true})
-        }
-      })
-    }
-  })
-}},
-deleteFood: function(name, callback) {
-  FoodModel.findOne({name: name}).exec(function(error, food) {
-    if (error) {
-      callback({error: true})
-    } else if (!food) {
-      callback({notFoundError: true})
-    } else {
-      food.remove(function(error) {
-        if (error) {
-          callback({error: true})
-        } else {
-          callback({success: true})
-        }
-      })
-    }
-  })
-}
+  changeFoodDescription: function (name, newDescription, callback) {
+    FoodModel.findOne({ name: name }).exec(function (error, food) {
+      if (error) {
+        callback({ error: true });
+      } else if (!food) {
+        callback({ notFoundError: true });
+      } else {
+        food.description = newDescription;
+
+        food.save(function (error) {
+          if (error) {
+            callback({ error: true });
+          } else {
+            callback({ success: true });
+          }
+        });
+      }
+    });
+  },
+
+  deleteFood: function (name, callback) {
+    FoodModel.findOne({ name: name }).exec(function (error, food) {
+      if (error) {
+        callback({ error: true });
+      } else if (!food) {
+        callback({ notFoundError: true });
+      } else {
+        food.remove(function (error) {
+          if (error) {
+            callback({ error: true });
+          } else {
+            callback({ success: true });
+          }
+        });
+      }
+    });
+  },
+};
--- src/routes/foods/index.js
+++ src/routes/foods/index.js
@@ -1,39 +1,55 @@
-const express = require("express")
+const express = require("express");
 
-const api = require("./api.js")
+const api = require("./api.js");
 
-const app = express.Router()
+const app = express.Router();
 
-app.post("/foods/create-new-food", function(request, response) {
-  if (!request.body.name || !request.body.calories || !request.body.description) {
-    response.json({error: true})
+app.post("/foods/create-new-food", function (request, response) {
+  if (
+    !request.body.name ||
+    !request.body.calories ||
+    !request.body.description
+  ) {
+    response.json({ error: true });
   } else {
-    api.createNewFood(request.body.name, request.body.calories, request.body.description, function(result) {
-      response.json(result) }
-    )})
+    api.createNewFood(
+      request.body.name,
+      request.body.calories,
+      request.body.description,
+      function (result) {
+        response.json(result);
+      }
+    );
+  }
+});
 
-app.get("/foods/get-all-foods", function(request, response) {
-  api.getAllFoods(function(result) {
-    response.json(result)
-  })})
+app.get("/foods/get-all-foods", function (request, response) {
+  api.getAllFoods(function (result) {
+    response.json(result);
+  });
+});
 
-  app.put("/foods/change-food-description", function(request, response) {
+app.put("/foods/change-food-description", function (request, response) {
   if (!request.body.name || !request.body.newDescription) {
-    response.json({error: true})
+    response.json({ error: true });
   } else {
-    api.changeFoodDescription(request.body.name, request.body.newDescription, function(result) {
-      response.json(result)
-    })
+    api.changeFoodDescription(
+      request.body.name,
+      request.body.newDescription,
+      function (result) {
+        response.json(result);
+      }
+    );
   }
-})
-app.delete("/foods/delete-food", function(request, response) {
+});
+app.delete("/foods/delete-food", function (request, response) {
   if (!request.body.name) {
-    response.json({error: true})
+    response.json({ error: true });
   } else {
-    api.deleteFood(request.body.name, function(result) {
-      response.json(result)
-    })
+    api.deleteFood(request.body.name, function (result) {
+      response.json(result);
+    });
   }
-})
+});
 
-module.exports = app
+module.exports = app;
